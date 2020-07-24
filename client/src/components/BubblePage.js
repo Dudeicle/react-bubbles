f500f9e7855a90932cf282e69719b8e9899dc643
Dudeicle <brian-griffiths@lambdastudents.com>
2020-07-24 12:53:20 -0400

    stuck on unknown error, project keeps freezing, tried resetting multiple times but no effect. waiting for assistance from TL

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -15,7 +15,6 @@ const BubblePage = () => {
 		axiosWithAuth()
 			.get('/colors')
 			.then((res) => {
-				console.log(res.data);
 				setColorList(res.data);
 			})
 			.catch((err) => console.log({ err }));
